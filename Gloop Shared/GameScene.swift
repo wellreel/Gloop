34e8d5775766cc143fba8c87b51762ae7f644a5e
Matthew Hundley <wellreel@>
2020-12-09 22:02:53 -0500

    character movement and animation

--- Gloop Shared/GameScene.swift
+++ Gloop Shared/GameScene.swift
@@ -9,29 +9,55 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    override func didMove(to view: SKView) {
-        //add background image
 
-        let background = SKSpriteNode(imageNamed: "background_1")
-        background.anchorPoint = CGPoint.zero
-        background.zPosition = Layer.background.rawValue
-        background.position = CGPoint.zero
-        addChild(background)
+  let player = Player()
+  let playerSpeed: CGFloat = 1.5
 
-        let foreground = SKSpriteNode(imageNamed: "foreground_1")
-        foreground.anchorPoint = CGPoint.zero
-        foreground.zPosition = Layer.foreground.rawValue
-        foreground.position = CGPoint.zero
-        addChild(foreground)
+  override func didMove(to view: SKView) {
 
-        let player = Player()
-        player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
-        addChild(player)
+    // Set up background
+    let background = SKSpriteNode(imageNamed: "background_1")
+    background.anchorPoint = CGPoint(x: 0, y: 0)
+    background.zPosition = Layer.background.rawValue
+    background.position = CGPoint(x: 0, y: 0)
+    addChild(background)
 
-        player.walk()
-    }
+    // Set up foreground
+    let foreground = SKSpriteNode(imageNamed: "foreground_1")
+    foreground.anchorPoint = CGPoint(x: 0, y: 0)
+    foreground.zPosition = Layer.foreground.rawValue
+    foreground.position = CGPoint(x: 0, y: 0)
+    addChild(foreground)
 
+    // Set up player
+    player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
+    player.setupConstraints(floor: foreground.frame.maxY)
+    addChild(player)
+    player.walk()
   }
+
+  // MARK: - TOUCH HANDLING
+
+  /* ############################################################ */
+  /*                 TOUCH HANDLERS STARTS HERE                   */
+  /* ############################################################ */
+
+  func touchDown(atPoint pos : CGPoint) {
+    // Calculate the speed based on current position and tap location
+    let distance = hypot(pos.x-player.position.x, pos.y-player.position.y)
+    let calculatedSpeed = TimeInterval(distance / playerSpeed) / 255
+    // print(" distance: \(distance) \n calculatedSpeed: \(calculatedSpeed)")
+
+    if pos.x < player.position.x {
+      player.moveToPosition(pos: pos, direction: "L", speed: calculatedSpeed)
+    } else {
+      player.moveToPosition(pos: pos, direction: "R", speed: calculatedSpeed)
+    }
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+  }
+}
 
 
--- Gloop Shared/Player.swift
+++ Gloop Shared/Player.swift
@@ -14,53 +14,71 @@ enum PlayerAnimationType: String {
 }
 
 class Player: SKSpriteNode {
-    // MARK: - properties
+  // MARK: - PROPERTIES
 
-    // Textures (Animation)
-    private var walkTextures: [SKTexture]?
+  // Textures (Animation)
+  private var walkTextures: [SKTexture]?
 
-    // MARK: - init
-    init() {
+  // MARK: - INIT
 
-      // Set default texture
-      let texture = SKTexture(imageNamed: "blob-walk_0")
+  init() {
 
-      // Call to super.init
-      super.init(texture: texture, color: .clear, size: texture.size())
+    // Set default texture
+    let texture = SKTexture(imageNamed: "blob-walk_0")
 
-      // Setup animation textures
-      self.walkTextures = self.loadTextures(atlas: "blob", prefix: "blob-walk_",
-                                            startsAt: 0, stopsAt: 2)
+    // Call to super.init
+    super.init(texture: texture, color: .clear, size: texture.size())
 
-      /* The call to the `loadTextures` extension essentially does this:
-      self.walkTextures = [SKTexture(imageNamed: "blob-walk_0"),
-                           SKTexture(imageNamed: "blob-walk_1"),
-                           SKTexture(imageNamed: "blob-walk_2")] */
+    // Setup animation textures
+    self.walkTextures = self.loadTextures(atlas: "blob", prefix: "blob-walk_",
+                                          startsAt: 0, stopsAt: 2)
 
-      // Setup other properties after init
-      self.name = "player"
-      self.setScale(1.0)
-      self.anchorPoint = CGPoint(x: 0.5, y: 0.0) // center-bottom
-      self.zPosition = Layer.player.rawValue
+    /* The call to the `loadTextures` extension essentially does this:
+    self.walkTextures = [SKTexture(imageNamed: "blob-walk_0"),
+                         SKTexture(imageNamed: "blob-walk_1"),
+                         SKTexture(imageNamed: "blob-walk_2")] */
+
+    // Setup other properties after init
+    self.name = "player"
+    self.setScale(1.0)
+    self.anchorPoint = CGPoint(x: 0.5, y: 0.0) // center-bottom
+    self.zPosition = Layer.player.rawValue
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - METHODS
+
+  func setupConstraints(floor: CGFloat) {
+    let range = SKRange(lowerLimit: floor, upperLimit: floor)
+    let lockToPlatform = SKConstraint.positionY(range)
+
+    constraints = [ lockToPlatform ]
+  }
+
+  func walk() {
+    // Check for textures
+    guard let walkTextures = walkTextures else {
+      preconditionFailure("Could not find textures!")
     }
 
-    required init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
+    // Run animation (forever)
+    startAnimation(textures: walkTextures, speed: 0.25,
+                   name: PlayerAnimationType.walk.rawValue,
+                   count: 0, resize: true, restore: true)
+  }
+
+  func moveToPosition(pos: CGPoint, direction: String, speed: TimeInterval) {
+    switch direction {
+    case "L":
+      xScale = -abs(xScale)
+    default:
+      xScale = abs(xScale)
     }
 
-    // MARK: - METHODS
-    func walk() {
-      // Check for textures
-      guard let walkTextures = walkTextures else {
-        preconditionFailure("Could not find textures!")
-      }
-
-      // Run animation (forever)
-      startAnimation(textures: walkTextures, speed: 0.25,
-                     name: PlayerAnimationType.walk.rawValue,
-                     count: 0, resize: true, restore: true)
-    }
+    let moveAction = SKAction.move(to: pos, duration: speed)
+    run(moveAction)
+  }
 }
-
-
-
