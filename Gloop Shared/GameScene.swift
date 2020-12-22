6326bfc9ff387f7346c8bfdcfa0b24d0673187f7
Matthew Hundley <wellreel@>
2020-12-21 22:02:19 -0500

    test

--- Gloop Shared/GameScene.swift
+++ Gloop Shared/GameScene.swift
@@ -10,74 +10,154 @@ import GameplayKit
 
 class GameScene: SKScene {
 
-  let player = Player()
-  let playerSpeed: CGFloat = 1.5
+    let player = Player()
+    let playerSpeed: CGFloat = 1.5
 
-  override func didMove(to view: SKView) {
+    // Player movement
+    var movingPlayer = false
+    var lastPosition: CGPoint?
 
-    // Set up background
-    let background = SKSpriteNode(imageNamed: "background_1")
-    background.anchorPoint = CGPoint(x: 0, y: 0)
-    background.zPosition = Layer.background.rawValue
-    background.position = CGPoint(x: 0, y: 0)
-    addChild(background)
+    var level: Int = 8
 
-    // Set up foreground
-    let foreground = SKSpriteNode(imageNamed: "foreground_1")
-    foreground.anchorPoint = CGPoint(x: 0, y: 0)
-    foreground.zPosition = Layer.foreground.rawValue
-    foreground.position = CGPoint(x: 0, y: 0)
-    addChild(foreground)
-
-    // Set up player
-    player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
-    player.setupConstraints(floor: foreground.frame.maxY)
-    addChild(player)
-    player.walk()
-
-    // Set up game
-    spawnGloop()
+    var numberOfDrops: Int = 10
+    var dropSpeed: CGFloat = 1.0
+    var minDropSpeed: CGFloat = 0.12
+    var maxDropSpeed: CGFloat = 1.0
 
 
-  }
+    override func didMove(to view: SKView) {
 
-  // MARK: - Game Functions
+        // Set up background
+        let background = SKSpriteNode(imageNamed: "background_1")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.zPosition = Layer.background.rawValue
+        background.position = CGPoint(x: 0, y: 0)
+        addChild(background)
+
+        // Set up foreground
+        let foreground = SKSpriteNode(imageNamed: "foreground_1")
+        foreground.anchorPoint = CGPoint(x: 0, y: 0)
+        foreground.zPosition = Layer.foreground.rawValue
+        foreground.position = CGPoint(x: 0, y: 0)
+        addChild(foreground)
+
+        // Set up player
+        player.position = CGPoint(x: size.width/2, y: foreground.frame.maxY)
+        player.setupConstraints(floor: foreground.frame.maxY)
+        addChild(player)
+        player.walk()
+
+        // Set up game
+        spawnMultipleGloops()
+
+    }
+
+    // MARK: - Game Functions
 
     /* ############################################################ */
     /*                 Game Functions STARTS HERE                   */
     /* ############################################################ */
 
+
+    func spawnMultipleGloops() {
+
+        // Set number of drops based on level
+        switch level {
+        case 1...5:
+            numberOfDrops = level * 10
+        case 6:
+            numberOfDrops = 75
+        case 7:
+            numberOfDrops = 100
+        case 8:
+            numberOfDrops = 150
+        default:
+            numberOfDrops = 150
+        }
+        // Set up drop speed
+        dropSpeed = 1 / (CGFloat(level) + (CGFloat(level) / CGFloat(numberOfDrops)))
+        if dropSpeed < minDropSpeed {
+            dropSpeed = minDropSpeed
+        } else if dropSpeed > maxDropSpeed {
+            dropSpeed = maxDropSpeed
+        }
+
+        // Set up repeating action
+        let wait = SKAction.wait(forDuration: TimeInterval(dropSpeed))
+        let spawn = SKAction.run { [unowned self] in self.spawnGloop()}
+        let sequence = SKAction.sequence([wait, spawn])
+        let repeatAction = SKAction.repeat(sequence, count: numberOfDrops)
+
+        // Run action
+        run(repeatAction, withKey: "gloop")
+    }
+
+
     func spawnGloop() {
         let collectible = Collectible(collectibleType: CollectibleType.gloop)
-        collectible.position = CGPoint(x: player.position.x, y: player.position.y * 2.5)
+
+        // Set random position
+        let margin = collectible.size.width * 2
+        let dropRange = SKRange(lowerLimit: frame.minX + margin, upperLimit: frame.maxX - margin)
+        let randomX = CGFloat.random(in: dropRange.lowerLimit...dropRange.upperLimit)
+
+        collectible.position = CGPoint(x: randomX, y: player.position.y * 2.5)
         addChild(collectible)
 
-        
         collectible.drop(dropSpeed: TimeInterval(1.0), floorLevel: player.frame.minY)
     }
 
-  // MARK: - TOUCH HANDLING
+    // MARK: - TOUCH HANDLING
 
-  /* ############################################################ */
-  /*                 TOUCH HANDLERS STARTS HERE                   */
-  /* ############################################################ */
+    /* ############################################################ */
+    /*                 TOUCH HANDLERS STARTS HERE                   */
+    /* ############################################################ */
 
-  func touchDown(atPoint pos : CGPoint) {
-    // Calculate the speed based on current position and tap location
-    let distance = hypot(pos.x-player.position.x, pos.y-player.position.y)
-    let calculatedSpeed = TimeInterval(distance / playerSpeed) / 255
-    // print(" distance: \(distance) \n calculatedSpeed: \(calculatedSpeed)")
-
-    if pos.x < player.position.x {
-      player.moveToPosition(pos: pos, direction: "L", speed: calculatedSpeed)
-    } else {
-      player.moveToPosition(pos: pos, direction: "R", speed: calculatedSpeed)
+    func touchDown(atPoint pos: CGPoint) {
+      let touchedNode = atPoint(pos)
+      if touchedNode.name == "player" {
+        movingPlayer = true
+      }
     }
-  }
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-  }
+    func touchMoved(toPoint pos: CGPoint) {
+      if movingPlayer == true {
+        // Clamp position
+        let newPos = CGPoint(x: pos.x, y: player.position.y)
+        player.position = newPos
+
+        // Check last position; if empty set it
+        let recordedPosition = lastPosition ?? player.position
+        if recordedPosition.x > newPos.x {
+          player.xScale = -abs(xScale)
+        } else {
+          player.xScale = abs(xScale)
+        }
+
+        // Save last known position
+        lastPosition = newPos
+      }
+    }
+
+    func touchUp(atPoint pos: CGPoint) {
+      movingPlayer = false
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+      for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
 }
 
 
